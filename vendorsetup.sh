13213f8fa6f4d677b49a718aca6903161f956bc3
sub77 <sub77@ymail.com>
2015-09-01 22:16:45 +0200

    moved patchdir to common

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Run device patches on the tree
-sh device/samsung/matisselte/patches/apply.sh
+#sh device/samsung/matisselte/patches/apply.sh
 
 add_lunch_combo cm_matisselte-eng
 add_lunch_combo cm_matisselte-userdebug
